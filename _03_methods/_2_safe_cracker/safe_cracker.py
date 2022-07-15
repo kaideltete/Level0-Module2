011227380c129db3356de3f236b441c57957e564
league <kaideltete@icloud.com>
2022-07-15 13:15:34 -0700

    lvl 0 mod 2

--- _03_methods/_2_safe_cracker/safe_cracker.py
+++ _03_methods/_2_safe_cracker/safe_cracker.py
@@ -1,13 +1,18 @@
 import random
-import sys
 from tkinter import messagebox, Tk, simpledialog
+import sys
+from tkinter import simpledialog, Tk
 from playsound import playsound
-
 
 def crack_the_safe():
     pass
     # TODO: Your mission: Use the try_code method to crack the safe
     #  by trying all possible combinations
+
+
+    for i in range(1000):
+        e=i+999000
+        try_code(e)
 
 # ======================= DO NOT EDIT THE CODE BELOW =========================
 
@@ -20,13 +25,13 @@ def try_code(guess):
     secret_code = 999999 - wekncrzpasfdkjhcfjse
 
     if guess == secret_code:
-        messagebox.showinfo(None, "Congratulations! You cracked the safe with " + str(guess))
+        messagebox.showinfo(None, "Congratulations! You cracked the safe with this amount of guesses\n" + str(guess))
         play_the_sound_of_success()
         sys.exit(0)
 
 
-def play_the_sound_of_success():
-    playsound('me-gusta.wav')
+#def play_the_sound_of_success():
+   # playsound('me-gusta.wav')
 
 
 if __name__ == '__main__':
